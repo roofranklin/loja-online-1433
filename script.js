539622711f17b339129b4332edcf489ff21637ba
Roosevelt Franklin <roosevelt.franklin@chatguru.com.br>
2025-10-01 21:30:13 -0300

    aula 05 finalizada

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const catalogoProduto = document.querySelector('#catalogo');
 const formProduto = document.getElementById('form-produto');
 const inputTitulo = document.getElementById('input-titulo');
 const inputPreco = document.getElementById('input-preco');
+const destaqueProduto = document.getElementById('destaque');
 
 let listaDeProdutos = [];
 
@@ -38,15 +39,60 @@ function salvarProdutosNoStorage() {
     localStorage.setItem('produtos', JSON.stringify(listaDeProdutos));
 }
 
-function buscarProdutosDaApi() {
-    fetch('https://fakestoreapi.com/products')
-    .then(response => response.json())
-    .then(produtos => {
+// function buscarProdutosDaApi() {
+//     fetch('https://fakestoreapi.com/products')
+//     .then(response => response.json())
+//     .then(produtos => {
+//         listaDeProdutos = produtos;
+//         renderizarProdutos();
+//         salvarProdutosNoStorage();
+
+//         const primeiroProdutoId = produtos[0].id;
+
+//         // CÓDIGO FICANDO ANINHADO
+//         fetch(`https://fakestoreapi.com/products/${primeiroProdutoId}`)
+//         .then(response => response.json())
+//         .then(destaqueProduto => {
+//             console.log("Produto em Destaque: ", destaqueProduto);
+//         });
+//     })
+//     .catch(erro => console.error('Erro ao buscar os produtos:', erro));
+// }
+
+// CÓDIGO DE BUSCA DA API REFATORADO
+async function carregarProdutos() {
+    try {
+        const response = await fetch('https://fakestoreapi.com/products');
+        if(!response.ok) {
+            throw new Error(`Erro HTTP: ${response.status}`);
+        }
+        const produtos = await response.json();
         listaDeProdutos = produtos;
+
+        // Limpa os containers antes de renderizar
+        destaqueProduto.innerHTML = '';
+        catalogoProduto.innerHTML = '';
+
+        if(produtos.length > 0){
+
+            const primeiroProdutoId = produtos[0].id;
+            const responseDestaque = await fetch(`https://fakestoreapi.com/products/${primeiroProdutoId}`);
+            if(!response.ok){
+                throw new Error(`Erro HTTP ao buscar destaque: ${responseDestaque.status}`);
+            }
+            const produtoDestaque = await responseDestaque.json();
+
+            const cardDestaque = criarCardProduto(produtoDestaque);
+            destaqueProduto.appendChild(cardDestaque);
+        }
+
         renderizarProdutos();
         salvarProdutosNoStorage();
-    })
-    .catch(erro => console.error('Erro ao buscar os produtos:', erro));
+    }
+    catch (erro) {
+        console.error("Ocorreu um erro na busca de produtos: ", erro);
+        catalogoProduto.innerHTML = '<p class="error">Não foi possível carregar os produtos!</p>'
+    }
 }
 
 formProduto.addEventListener('submit', function(event){
@@ -74,8 +120,17 @@ window.onload = function() {
     const produtosSalvos = localStorage.getItem('produtos');
     if (produtosSalvos) {
         listaDeProdutos = JSON.parse(produtosSalvos);
+
+        // Renderiza o primeiro produto a partir do storage
+        if(listaDeProdutos.length > 0) {
+            destaqueProduto.innerHTML = '';
+            const cardDestaque = criarCardProduto(listaDeProdutos[0]);
+            destaqueProduto.appendChild(cardDestaque);
+        }
+
+        // Renderiza o restante do catálogo
         renderizarProdutos();
     } else {
-        buscarProdutosDaApi(); // Só busca se tiver vazio
+        carregarProdutos(); // Só busca se tiver vazio
     }
 }
